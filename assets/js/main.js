62c38584db1065be005a74cbf930bfe7f108528f
Dylan Hughes <dwh183@hotmail.com>
2023-05-22 09:51:09 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,60 +199,60 @@
 	// Intro.
 		var $intro = $('#intro');
 
-		if ($intro.length > 0) {
+		//if ($intro.length > 0) {
 
 			// Hack: Fix flex min-height on IE.
-				if (browser.name == 'ie') {
-					$window.on('resize.ie-intro-fix', function() {
+				//if (browser.name == 'ie') {
+				//	$window.on('resize.ie-intro-fix', function() {
 
-						var h = $intro.height();
+				//		var h = $intro.height();
 
-						if (h > $window.height())
-							$intro.css('height', 'auto');
-						else
-							$intro.css('height', h);
+				//		if (h > $window.height())
+				//			$intro.css('height', 'auto');
+				//		else
+				//			$intro.css('height', h);
 
-					}).trigger('resize.ie-intro-fix');
-				}
+				//	}).trigger('resize.ie-intro-fix');
+				//}
 
 			// Hide intro on scroll (> small).
-				breakpoints.on('>small', function() {
+				//breakpoints.on('>small', function() {
 
-					$main.unscrollex();
+				//	$main.unscrollex();
 
-					$main.scrollex({
-						mode: 'bottom',
-						top: '25vh',
-						bottom: '-50vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
+				//	$main.scrollex({
+				//		mode: 'bottom',
+				//		top: '25vh',
+				//		bottom: '-50vh',
+				//		enter: function() {
+				//			$intro.addClass('hidden');
+				//		},
+				//		leave: function() {
+				//			$intro.removeClass('hidden');
+				//		}
+				//	});
 
-				});
+				//});
 
 			// Hide intro on scroll (<= small).
-				breakpoints.on('<=small', function() {
+				//breakpoints.on('<=small', function() {
 
-					$main.unscrollex();
+				//	$main.unscrollex();
 
-					$main.scrollex({
-						mode: 'middle',
-						top: '15vh',
-						bottom: '-15vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
+				//	$main.scrollex({
+				//		mode: 'middle',
+				//		top: '15vh',
+				//		bottom: '-15vh',
+				//		enter: function() {
+				//			$intro.addClass('hidden');
+				//		},
+				//		leave: function() {
+				//			$intro.removeClass('hidden');
+				//		}
+				//	});
 
-			});
+			//});
 
-		}
+		//}
 
 })(jQuery);
